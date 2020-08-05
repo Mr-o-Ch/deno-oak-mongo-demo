aa2d547cfb2902aea73d44541d1f3cb50e234c78
杨俊宁 <youngjuning@aliyun.com>
2020-08-06 00:46:42 +0800

    更新 denv 用法

--- server.ts
+++ server.ts
@@ -1,7 +1,9 @@
-import "https://deno.land/x/denv/mod.ts";
+import {load} from "https://deno.land/x/denv/mod.ts";
 import { Application } from "./deps.ts";
 import router from "./router.ts";
 import errorMiddleware from "./middlewares/error.ts";
+
+await load()
 
 const APP_NAME = Deno.env.get("APP_NAME") || "oak";
 const APP_HOST = Deno.env.get("APP_HOST") || "127.0.0.1";
